d3f9aa8672d2509335ffe734e49d58a6880c4d2d
alisrios <alisrios@gmail.com>
2025-02-04 09:58:45 -0300

    Ajustando terraform para trabalhar com assume role

--- state_config.tf
+++ state_config.tf
@@ -2,6 +2,9 @@ terraform {
   backend "s3" {
     bucket  = "terraform-bia"
     key     = "terraform.tfstate"
-    region  = "us-east-1"    
+    region  = "us-east-1"
+    assume_role {
+      role_arn = "arn:aws:iam::148761658767:role/TerraformAssumeRole"
+    }
   }
 }
