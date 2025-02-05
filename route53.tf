0b8ed1fa2e6d9f0e093f3e8e69c211be0482767e
alisrios <alisrios@gmail.com>
2025-02-05 08:08:11 -0300

    Adicionado route53

---
+++ route53.tf
@@ -0,0 +1,11 @@
+data "aws_route53_zone" "primary" {
+  name = "alisriosti.com.br"
+}
+
+resource "aws_route53_record" "bia_tf" {
+  zone_id = data.aws_route53_zone.primary.zone_id  # Substitua pelo ID correto da sua zona
+  name    = "bia-tf.alisriosti.com.br"
+  type    = "CNAME"
+  ttl     = 300
+  records = [aws_cloudfront_distribution.bia_tf.domain_name]
+}
