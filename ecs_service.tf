1b36c2a9ab6c00e42d438528ee169c640addd109
alisrios <alisrios@gmail.com>
2025-02-16 21:56:06 -0300

    Ajustando o service

--- ecs_service.tf
+++ ecs_service.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_service" "bia-tf" {
   name            = "service-bia-tf"
   cluster         = aws_ecs_cluster.cluster_bia_tf.id
   task_definition = aws_ecs_task_definition.bia_web.arn
-  desired_count   = 1
+  desired_count   = 2
 
   capacity_provider_strategy {
     capacity_provider = aws_ecs_capacity_provider.bia_tf.name
@@ -14,8 +14,17 @@ resource "aws_ecs_service" "bia-tf" {
     type  = "spread"
     field = "attribute:ecs.availability-zone"
   }
-  deployment_minimum_healthy_percent = 0
+  deployment_minimum_healthy_percent = 50
   deployment_maximum_percent         = 100
+
+  depends_on = [aws_lb_target_group.tg_bia_tf]
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg_bia_tf.arn
+    container_name   = "bia-tf"
+    container_port   = 8080
+  }
+
 
   lifecycle {
     ignore_changes = [desired_count]
