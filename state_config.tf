87ef79fbb73c4bedc1ed376358997667bc7a7368
alisrios <alisrios@gmail.com>
2025-04-04 11:39:06 -0300

    Ajuste de README.md e state locking no S3

--- state_config.tf
+++ state_config.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-bia"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile = true       
+    bucket       = "terraform-bia"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
